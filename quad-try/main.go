024cae20932b5bb86e1bc70d884651be4b63da20
folorunn <gamp@gamp-HP-EliteBook-840-G8-Notebook-PC>
2026-06-02 13:39:56 +0100

    Updated

---
+++ quad-try/main.go
@@ -0,0 +1,10 @@
+package main
+import "quad/piscine"
+
+func main() {
+	 piscine.QuadA(5, 3)
+	 piscine.QuadB(5, 3)
+	 piscine.QuadC(5, 3)
+	 piscine.QuadD(5, 3)
+	 piscine.QuadE(5, 3)
+}
---
+++ quad-try/piscine/quadC.go
@@ -0,0 +1,44 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func QuadC(x, y int) {
+	if x <= 0 || y <= 0 {
+		return
+	}
+
+	for i := 1; i <= y; i++ {
+		for j := 1; j <= x; j++ {
+
+			
+			if i == 1 {
+				if j == 1 {
+					z01.PrintRune('A')
+				} else if j == x {
+					z01.PrintRune('A')
+				} else {
+					z01.PrintRune('B')
+				}
+
+			
+			} else if i == y {
+				if j == 1 {
+					z01.PrintRune('C')
+				} else if j == x {
+					z01.PrintRune('C')
+				} else {
+					z01.PrintRune('B')
+				}
+
+			
+			} else {
+				if j == 1 || j == x {
+					z01.PrintRune('B')
+				} else {
+					z01.PrintRune(' ')
+				}
+			}
+		}
+		z01.PrintRune('\n')
+	}
+}
